5cd4c969b9a7d648b6416310a72d86935d0fcc42
Munir Wanis <fallen.angel.pp@gmail.com>
2017-03-15 11:20:49 -0300

    :art: First Commit

---
+++ Infnet.Aspnet.Tp3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Infnet.Aspnet.Tp3.MvcApplication" Language="C#" %>
---
+++ Infnet.Aspnet.Tp3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Infnet.Aspnet.Tp3.Startup))]
+namespace Infnet.Aspnet.Tp3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
