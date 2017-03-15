c322ca84724021e42cb94af129685d5135c74723
Munir Wanis <fallen.angel.pp@gmail.com>
2017-03-15 11:50:03 -0300

    :construction: Adds book model, view and controller

---
+++ Infnet.Aspnet.Tp3/App_Data/dbo.Books.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Books]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] VARCHAR(255) NOT NULL, 
+    [Author] VARCHAR(255) NOT NULL, 
+    [Publisher] VARCHAR(255) NOT NULL, 
+    [Year] INT NOT NULL
+)
---
+++ Infnet.Aspnet.Tp3/Models/BookViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infnet.Aspnet.Tp3.Models
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+
+        [StringLength(255, ErrorMessage = "Max length is 255 caracters", MinimumLength = 2), Required]
+        public string Author { get; set; }
+
+        [StringLength(255, ErrorMessage = "Max length is 255 caracters", MinimumLength = 2), Required]
+        public string Publisher { get; set; }
+
+        [StringLength(255, ErrorMessage = "Max length is 255 caracters", MinimumLength = 2), Required]
+        public string Title { get; set; }
+
+        [Range(0, 2030, ErrorMessage = "The year is not valid"), Required]
+        public int Year { get; set; }
+    }
+}
