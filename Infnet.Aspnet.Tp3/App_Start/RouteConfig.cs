cbd65d7f462b63f9f832e8f8c6e7b142ddac92e7
Munir Wanis <fallen.angel.pp@gmail.com>
2017-03-23 10:49:52 -0300

    :construction: Changes route to default books

--- Infnet.Aspnet.Tp3/App_Start/RouteConfig.cs
+++ Infnet.Aspnet.Tp3/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Infnet.Aspnet.Tp3
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Books", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
