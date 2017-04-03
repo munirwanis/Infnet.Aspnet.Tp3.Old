03d4c40493b083f5fa91d6d409f2350953a73e83
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-03 12:12:32 -0300

    Put Loan table

---
+++ Infnet.Aspnet.Tp3/App_Data/dbo.Loan.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Loan]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [LoanDate] DATETIME NOT NULL, 
+    [DevolutionDate] DATETIME NOT NULL, 
+    [BookId] INT NOT NULL, 
+    CONSTRAINT [FK_Loan_Book] FOREIGN KEY ([BookId]) REFERENCES [Books]([Id])
+)
